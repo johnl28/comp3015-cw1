b4874f45a5d78b99d4f7ca7a7057de48f1189b26
Johnl <ionutcriste28@gmail.com>
2024-03-22 20:06:18 +0000

    Add Pitch and Yaw

--- helper/Camera.h
+++ helper/Camera.h
@@ -10,10 +10,19 @@ struct Camera
 	glm::vec3 CameraPos = { 0.0f, 0.0f, 3.0f };
 	glm::vec3 CameraUp = { 0.0f, 1.0f, 0.0f };
 
+	float Speed = 0.05f;
+	float Pitch = 0.0f;
+	float Yaw = -90.0f;
 
 
 	glm::mat4 GetView()
 	{
+		glm::vec3 direction;
+		direction.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		direction.y = sin(glm::radians(Pitch));
+		direction.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		CameraFront = glm::normalize(direction);
+
 		return glm::lookAt(CameraPos, CameraPos + CameraFront, CameraUp);
 	}
 };
